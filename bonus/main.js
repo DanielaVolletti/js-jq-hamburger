7176774e3041299bbbac44df70e9c60d9821ae07
Daniela Maria Volletti <danielavolletti@gmail.com>
2020-03-31 19:41:32 +0200

    bonus

---
+++ bonus/main.js
@@ -0,0 +1,13 @@
+// faccio apparire menu hamburger
+$("#icona-menu-hamburger").click(
+  function() {
+    $("#hamburgher").fadeIn();
+  }
+);
+
+// click chiusura menu
+$("#chiudi-hamburger").click(
+  function() {
+    $("#hamburgher").fadeOut();
+  }
+);
